63ccdb32a7b130502bfcd7fb2dc4bef5af7348e4
Adrian <ar.hordyk@gmail.com>
2020-10-13 12:50:15 -0700

    Update Data_Functions.R

--- R/Data_Functions.R
+++ R/Data_Functions.R
@@ -35,15 +35,18 @@ replic8 <- function(Data, nrep) {
   
   for (sl in 1:length(slotnam)) {
     slt <- attr(Data, slotnam[sl])
-    if (inherits(slt, "matrix")) {
-      attr(Data, slotnam[sl]) <- matrix(rep(slt, each = nrep), 
-                                        nrow = nrep, ncol = ncol(slt))
-    } else if (inherits(slt, "numeric")) {
-      attr(Data, slotnam[sl]) <- rep(slt, nrep)
-    } else if (inherits(slt, "array")) {
-      attr(Data, slotnam[sl]) <- array(rep(slt, each = nrep), 
-                                       dim = c(nrep, dim(slt)[2:3]))
+    if (length(slt)>0) {
+      if (inherits(slt, "matrix")) {
+        attr(Data, slotnam[sl]) <- matrix(rep(slt, each = nrep), 
+                                          nrow = nrep, ncol = ncol(slt))
+      } else if (inherits(slt, "numeric")) {
+        attr(Data, slotnam[sl]) <- rep(slt, nrep)
+      } else if (inherits(slt, "array")) {
+        attr(Data, slotnam[sl]) <- array(rep(slt, each = nrep), 
+                                         dim = c(nrep, dim(slt)[2:3]))
+      }
     }
+  
   }
   Data
 }
