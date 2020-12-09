631a763a139bd93e0c1716a6fc2b634dc4bf52d6
Adrian <ar.hordyk@gmail.com>
2020-12-09 13:12:20 -0800

    Update Misc_Exported.R

--- R/Misc_Exported.R
+++ R/Misc_Exported.R
@@ -52,8 +52,12 @@ avail <- function(classy, package=NULL, msg=TRUE) {
   } else {
 
     packages <- c('MSEtool', 'SAMtool', 'DLMtool', 'DLMextra')
-    if (is.null(package))
+    if (is.null(package)) {
       package <- packages
+      pkgs <- search()
+      search_package <- paste0("package:",package)
+      package <- package[search_package %in% pkgs]
+    }
 
     global_funs <- ls(envir = .GlobalEnv)[vapply(ls(envir = .GlobalEnv), getclass, logical(1), classy = classy)]
     temp <- global_funs
@@ -782,10 +786,10 @@ ML2D <- function(OM, ML, nsim = 100, ploty = T, Dlim = c(0.05, 0.6)) {
   srs <- (maxage - AFS) / ((-log(Vmaxage,2))^0.5) # selectivity parameters are constant for all years
   sls <- (AFS - age05) /((-log(0.05,2))^0.5)
 
-  vuln <- t(sapply(1:nsim2, getsel, lens=matrix(1:maxage, nrow=nsim2, ncol=maxage, byrow=TRUE),
+  vuln <- t(sapply(1:nsim2, getsel, lens=matrix(1:maxage, nrow=nsim2, ncol=maxage+1, byrow=TRUE),
                    lfs=AFS, sls=sls, srs=srs))
 
-  Agearray <- array(rep(1:maxage, each = nsim2), c(nsim2, maxage))
+  Agearray <- array(rep(0:maxage, each = nsim2), c(nsim2, maxage+1))
   mat <- 1/(1 + exp((AM - (Agearray))/(AM * 0.1)))  # Maturity at age array
 
   nyears <- OM@nyears
@@ -817,136 +821,136 @@ ML2D <- function(OM, ML, nsim = 100, ploty = T, Dlim = c(0.05, 0.6)) {
 # Composition stock reduction analysis
 
 
-# #' Catch at size reduction analysis
-# #'
-# #' What depletion level and corresponding equlibrium F arise from data
-# #' regarding mean length of current catches, natural mortality rate, steepness
-# #' of the stock recruitment curve, maximum length, maximum growth rate, age at
-# #' maturity, age based vulnerability, maturity at age, maximum age and number
-# #' of historical years of fishing.
-# #'
-# #'
-# #' @usage CSRA(M,h,Linf,K,t0,AM,a,b,vuln,mat,ML,CAL,CAA,maxage,nyears)
-# #' @param M A vector of natural mortality rate estimates
-# #' @param h A vector of sampled steepness (Beverton-Holt stock recruitment)
-# #' @param Linf A vector of maximum length (von Bertalanffy growth)
-# #' @param K A vector of maximum growth rate (von Bertalanffy growth)
-# #' @param t0 A vector of theoretical age at length zero (von Bertalanffy
-# #' growth)
-# #' @param AM A vector of age at maturity
-# #' @param a Length-weight conversion parameter a (W=aL^b)
-# #' @param b Length-weight conversion parameter b (W=aL^b)
-# #' @param vuln A matrix nsim x nage of the vulnerabilty at age (max 1) to
-# #' fishing.
-# #' @param mat A matrix nsim x nage of the maturity at age (max 1)
-# #' @param ML A vector of current mean length estimates
-# #' @param CAL A catch-at-length matrix nyears x (1 Linf unit) length bins
-# #' @param CAA A catch-at-age matrix nyears x maximum age
-# #' @param maxage Maximum age
-# #' @param nyears Number of historical years of fishing
-# #' @author T. Carruthers
-# #' @export CSRA
-# #' @keywords internal
-# CSRA <- function(M, h, Linf, K, t0, AM, a, b, vuln, mat, ML, CAL, CAA,
-#                  maxage, nyears) {
-#   nsim <- length(M)
-#   Dep <- rep(NA, nsim)
-#   Fm <- rep(NA, nsim)
-#   for (i in 1:nsim) {
-#     fit <- optimize(CSRAfunc, log(c(1e-04, 5)), Mc = M[i], hc = h[i],
-#                     maxage, nyears, Linfc = Linf[i], Kc = K[i], t0c = t0[i], AMc = AM[i],
-#                     ac = a, bc = b, vulnc = vuln[i, ], matc = mat[i, ], MLc = ML[i],
-#                     CAL = NA, CAA = NA, opt = T)
-#
-#
-#     out <- CSRAfunc(fit$minimum, Mc = M[i], hc = h[i], maxage, nyears,
-#                     Linfc = Linf[i], Kc = K[i], t0c = t0[i], AMc = AM[i], ac = a,
-#                     bc = b, vulnc = vuln[i, ], matc = mat[i, ], MLc = ML[i], CAL = NA,
-#                     CAA = NA, opt = 3)
-#
-#     Dep[i] <- out[1]
-#     Fm[i] <- out[2]
-#
-#
-#   }
-#   cbind(Dep, Fm)
-# }
+#' Catch at size reduction analysis
+#'
+#' What depletion level and corresponding equlibrium F arise from data
+#' regarding mean length of current catches, natural mortality rate, steepness
+#' of the stock recruitment curve, maximum length, maximum growth rate, age at
+#' maturity, age based vulnerability, maturity at age, maximum age and number
+#' of historical years of fishing.
+#'
+#'
+#' @param M A vector of natural mortality rate estimates
+#' @param h A vector of sampled steepness (Beverton-Holt stock recruitment)
+#' @param Linf A vector of maximum length (von Bertalanffy growth)
+#' @param K A vector of maximum growth rate (von Bertalanffy growth)
+#' @param t0 A vector of theoretical age at length zero (von Bertalanffy
+#' growth)
+#' @param AM A vector of age at maturity
+#' @param a Length-weight conversion parameter a (W=aL^b)
+#' @param b Length-weight conversion parameter b (W=aL^b)
+#' @param vuln A matrix nsim x nage of the vulnerabilty at age (max 1) to
+#' fishing.
+#' @param mat A matrix nsim x nage of the maturity at age (max 1)
+#' @param ML A vector of current mean length estimates
+#' @param CAL A catch-at-length matrix nyears x (1 Linf unit) length bins
+#' @param CAA A catch-at-age matrix nyears x maximum age
+#' @param maxage Maximum age
+#' @param nyears Number of historical years of fishing
+#' @author T. Carruthers
+#' @export CSRA
+#' @keywords internal
+CSRA <- function(M, h, Linf, K, t0, AM, a, b, vuln, mat, ML, CAL, CAA,
+                 maxage, nyears) {
+  nsim <- length(M)
+  Dep <- rep(NA, nsim)
+  Fm <- rep(NA, nsim)
+  for (i in 1:nsim) {
+    fit <- optimize(CSRAfunc, log(c(1e-04, 5)), Mc = M[i], hc = h[i],
+                    maxage, nyears, Linfc = Linf[i], Kc = K[i], t0c = t0[i], AMc = AM[i],
+                    ac = a, bc = b, vulnc = vuln[i, ], matc = mat[i, ], MLc = ML[i],
+                    CAL = NA, CAA = NA, opt = T)
 
-# # The function that CSRA operates on
-#
-# #' Optimization function for CSRA
-# #'
-# #' What depletion level and corresponding equlibrium F arise from data
-# #' regarding mean length of current catches, natural mortality rate, steepness
-# #' of the stock recruitment curve, maximum length, maximum growth rate, age at
-# #' maturity, age based vulnerability, maturity at age, maximum age and number
-# #' of historical years of fishing.
-# #'
-# #'
-# #' @param lnF A proposed value of current instantaneous fishing mortality rate
-# #' @param Mc Natural mortality rate estimates
-# #' @param hc Steepness (Beverton-Holt stock recruitment)
-# #' @param maxage Maximum age
-# #' @param nyears Number of historical years of fishing
-# #' @param AFSc Age at full selection
-# #' @param AFCc Age at first capture
-# #' @param Linfc Maximum length (von Bertalanffy growth)
-# #' @param Kc Maximum growth rate (von Bertalanffy growth)
-# #' @param t0c Theoretical age at length zero (von Bertalanffy growth)
-# #' @param AMc Age at maturity
-# #' @param ac Length-weight conversion parameter a (W=aL^b)
-# #' @param bc Length-weight conversion parameter b (W=aL^b)
-# #' @param vulnc A vector (nage long) of the vulnerabilty at age (max 1) to
-# #' fishing.
-# #' @param matc A vector (nage long) of the maturity at age (max 1)
-# #' @param MLc A current mean length estimates
-# #' @param CAL A catch-at-length matrix nyears x (1 Linf unit) length bins
-# #' @param CAA A catch-at-age matrix nyears x maximum age
-# #' @param opt Should the measure of fit be returned?
-# #' @param meth Are we fitting to mean length or catch composition?
-# #' @author T. Carruthers
-# #' @keywords internal
-# CSRAfunc <- function(lnF, Mc, hc, maxage, nyears, AFSc, AFCc, Linfc, Kc,
-#                      t0c, AMc, ac, bc, vulnc, matc, MLc, CAL, CAA, opt = T, meth = "ML") {
-#
-#   Fm <- exp(lnF)
-#   Fc <- vulnc * Fm
-#   Lac <- Linfc * (1 - exp(-Kc * ((1:maxage) - t0c)))
-#   Wac <- ac * Lac^bc
-#   N <- exp(-Mc * ((1:maxage) - 1))
-#   SSN <- matc * N  # Calculate initial spawning stock numbers
-#   Biomass <- N * Wac
-#   SSB <- SSN * Wac  # Calculate spawning stock biomass
-#
-#   B0 <- sum(Biomass)
-#   SSB0 <- sum(SSB)
-#   SSN0 <- SSN
-#   SSBpR <- sum(SSB)  # Calculate spawning stock biomass per recruit
-#   SSNpR <- SSN
-#   Zc <- Fc + Mc
-#   CN <- array(NA, dim = c(nyears, maxage))
-#   HR <- rep(0, maxage)
-#   pen <- 0
-#   for (y in 1:nyears) {
-#     VB <- Biomass * vulnc * exp(-Mc)
-#     CN[y, ] <- N * (1 - exp(-Zc)) * (Fc/Zc)
-#     N[2:maxage] <- N[1:(maxage - 1)] * exp(-Zc[1:(maxage - 1)])  # Total mortality
-#     N[1] <- (0.8 * hc * sum(SSB))/(0.2 * SSBpR * (1 - hc) + (hc - 0.2) *
-#                                      sum(SSB))  # Recruitment assuming regional R0 and stock wide steepness
-#     Biomass <- N * Wac
-#     SSN <- N * matc
-#     SSB <- SSN * Wac
-#   }  # end of year
-#
-#   pred <- sum((CN[nyears, ] * Lac))/sum(CN[nyears, ])
-#   fobj <- (pred - MLc)^2  # Currently a least squares estimator. Probably not worth splitting hairs WRT likelihood functions!
-#   if (opt == 1) {
-#     return(fobj)
-#   } else {
-#     c(sum(SSB)/sum(SSB0), Fm)
-#   }
-# }
-#
+
+    out <- CSRAfunc(fit$minimum, Mc = M[i], hc = h[i], maxage, nyears,
+                    Linfc = Linf[i], Kc = K[i], t0c = t0[i], AMc = AM[i], ac = a,
+                    bc = b, vulnc = vuln[i, ], matc = mat[i, ], MLc = ML[i], CAL = NA,
+                    CAA = NA, opt = 3)
+
+    Dep[i] <- out[1]
+    Fm[i] <- out[2]
+
+
+  }
+  cbind(Dep, Fm)
+}
+
+# The function that CSRA operates on
+
+#' Optimization function for CSRA
+#'
+#' What depletion level and corresponding equlibrium F arise from data
+#' regarding mean length of current catches, natural mortality rate, steepness
+#' of the stock recruitment curve, maximum length, maximum growth rate, age at
+#' maturity, age based vulnerability, maturity at age, maximum age and number
+#' of historical years of fishing.
+#'
+#'
+#' @param lnF A proposed value of current instantaneous fishing mortality rate
+#' @param Mc Natural mortality rate estimates
+#' @param hc Steepness (Beverton-Holt stock recruitment)
+#' @param maxage Maximum age
+#' @param nyears Number of historical years of fishing
+#' @param AFSc Age at full selection
+#' @param AFCc Age at first capture
+#' @param Linfc Maximum length (von Bertalanffy growth)
+#' @param Kc Maximum growth rate (von Bertalanffy growth)
+#' @param t0c Theoretical age at length zero (von Bertalanffy growth)
+#' @param AMc Age at maturity
+#' @param ac Length-weight conversion parameter a (W=aL^b)
+#' @param bc Length-weight conversion parameter b (W=aL^b)
+#' @param vulnc A vector (nage long) of the vulnerabilty at age (max 1) to
+#' fishing.
+#' @param matc A vector (nage long) of the maturity at age (max 1)
+#' @param MLc A current mean length estimates
+#' @param CAL A catch-at-length matrix nyears x (1 Linf unit) length bins
+#' @param CAA A catch-at-age matrix nyears x maximum age
+#' @param opt Should the measure of fit be returned?
+#' @param meth Are we fitting to mean length or catch composition?
+#' @author T. Carruthers
+#' @keywords internal
+CSRAfunc <- function(lnF, Mc, hc, maxage, nyears, AFSc, AFCc, Linfc, Kc,
+                     t0c, AMc, ac, bc, vulnc, matc, MLc, CAL, CAA, opt = T, meth = "ML") {
+
+  Fm <- exp(lnF)
+  Fc <- vulnc * Fm
+  Lac <- Linfc * (1 - exp(-Kc * ((0:maxage) - t0c)))
+  Wac <- ac * Lac^bc
+  N <- exp(-Mc * ((0:maxage) - 1))
+  SSN <- matc * N  # Calculate initial spawning stock numbers
+  Biomass <- N * Wac
+  SSB <- SSN * Wac  # Calculate spawning stock biomass
+
+  B0 <- sum(Biomass)
+  SSB0 <- sum(SSB)
+  SSN0 <- SSN
+  SSBpR <- sum(SSB)  # Calculate spawning stock biomass per recruit
+  SSNpR <- SSN
+  Zc <- Fc + Mc
+  n_age <- maxage+1
+  CN <- array(NA, dim = c(nyears, n_age))
+  HR <- rep(0, n_age)
+  pen <- 0
+  for (y in 1:nyears) {
+    VB <- Biomass * vulnc * exp(-Mc)
+    CN[y, ] <- N * (1 - exp(-Zc)) * (Fc/Zc)
+    N[2:n_age] <- N[1:(n_age - 1)] * exp(-Zc[1:(n_age - 1)])  # Total mortality
+    N[1] <- (0.8 * hc * sum(SSB))/(0.2 * SSBpR * (1 - hc) + (hc - 0.2) *
+                                     sum(SSB))  # Recruitment assuming regional R0 and stock wide steepness
+    Biomass <- N * Wac
+    SSN <- N * matc
+    SSB <- SSN * Wac
+  }  # end of year
+
+  pred <- sum((CN[nyears, ] * Lac))/sum(CN[nyears, ])
+  fobj <- (pred - MLc)^2  # Currently a least squares estimator. Probably not worth splitting hairs WRT likelihood functions!
+  if (opt == 1) {
+    return(fobj)
+  } else {
+    c(sum(SSB)/sum(SSB0), Fm)
+  }
+}
+
 
 # Stochastic inverse growth curve used to back-calculate age at first
 # capture from length at first capture
