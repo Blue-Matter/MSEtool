e556c6511ab42942ade6405adeba596027d74dd9
Adrian <ar.hordyk@gmail.com>
2020-12-15 13:09:41 -0800

    Create test-runMSE.R

---
+++ tests/manual/test-code/test-runMSE.R
@@ -0,0 +1,34 @@
+
+
+testthat::context("Quick test of runMSE function")
+
+library(MSEtool)
+
+OM <- MSEtool::testOM
+OM@qinc <- OM@qcv <- c(0,0)
+OM@interval <- 1
+
+Hist <- Simulate(OM)
+
+testthat::expect_s4_class(Hist, 'Hist')
+
+curEref <- function(x, Data, ...) {
+  Rec <- new('Rec')
+  Rec@Effort <- 1
+  Rec
+}
+class(curEref) <- 'MP'
+
+MSE <- Project(Hist, MPs=c('FMSYref', 'curEref'))
+
+testthat::expect_s4_class(MSE, 'MSE')
+
+histF <- array(MSE@FM_hist[,OM@nyears], dim=dim(MSE@FM[,2,]))
+
+# curEref should result in projection Fs = last historical F
+testthat::expect_equal(MSE@FM[,2,], histF)
+
+# FMSYref should result in F/FMSY = 1
+testthat::expect_equal(MSE@F_FMSY[,1,], array(1, dim=dim(MSE@F_FMSY[,1,])))
+
+
