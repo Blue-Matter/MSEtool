47a065fb567c4a1da6cc2917b83b15eb1ac8a36e
Adrian <ar.hordyk@gmail.com>
2021-01-07 12:12:12 -0800

    update build tools

--- build_tools/Organize_Functions.R
+++ build_tools/Organize_Functions.R
@@ -6,21 +6,21 @@ DF$Class <- DF$File <- DF$Family <- NA
 
 for (i in 1:nrow(DF)) {
   DF$Class[i] <- class(get(DF$Object[i]))
-  
+
   srcfile <- attr(attr(get(DF$Object[i]),"srcref"),"srcfile")
   if (!is.null(srcfile)) DF$File[i] <- basename(srcfile$filename)
-  
-  
+
+
   class <- DF$Class[i]
   if (class %in% c('Stock', 'Fleet', 'Obs', 'Imp', 'Data', 'MOM')) {
     DF$Family[i] <- paste('Example', class, 'Objects')
   }
 
-  if (class %in% c('PM')) {
+  if (class %in% 'PM') {
     DF$Family[i] <- paste('Example', class, 'Methods')
   }
-  
-  if (class %in% c('MP')) {
+
+  if (class %in% 'MP') {
     DF$Family[i] <- 'Reference MPs'
   }
 }
--- build_tools/RHub_CRAN_Test.R
+++ build_tools/RHub_CRAN_Test.R
@@ -6,15 +6,12 @@ library(rhub)
 validate_email()
 
 # check for CRAN 
-rhub:::default_cran_check_platforms('.')
+rhub::default_cran_check_platforms('.')
 chk <- check_for_cran('../MSEtool', email='ar.hordyk@gmail.com')
 
 
 # needs docker and bash installed for Windows
 chk <- local_check_linux(image = "rhub/fedora-clang-devel")
-
-
-
 
 
 v <- as.character(packageVersion("MSEtool"))
