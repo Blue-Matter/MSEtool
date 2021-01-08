ab5122588195945349ce981a0605e175e9fcd0a8
Adrian <ar.hordyk@gmail.com>
2021-01-07 16:45:08 -0800

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,24 +6,6 @@
 
 using namespace Rcpp;
 
-// bhnoneq_LL
-double bhnoneq_LL(NumericVector stpar, NumericVector year, NumericVector Lbar, NumericVector ss, double Linf, double K, double Lc, int nbreaks);
-RcppExport SEXP _MSEtool_bhnoneq_LL(SEXP stparSEXP, SEXP yearSEXP, SEXP LbarSEXP, SEXP ssSEXP, SEXP LinfSEXP, SEXP KSEXP, SEXP LcSEXP, SEXP nbreaksSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type stpar(stparSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type year(yearSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type Lbar(LbarSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type ss(ssSEXP);
-    Rcpp::traits::input_parameter< double >::type Linf(LinfSEXP);
-    Rcpp::traits::input_parameter< double >::type K(KSEXP);
-    Rcpp::traits::input_parameter< double >::type Lc(LcSEXP);
-    Rcpp::traits::input_parameter< int >::type nbreaks(nbreaksSEXP);
-    rcpp_result_gen = Rcpp::wrap(bhnoneq_LL(stpar, year, Lbar, ss, Linf, K, Lc, nbreaks));
-    return rcpp_result_gen;
-END_RCPP
-}
 // combine
 NumericVector combine(const List& list);
 RcppExport SEXP _MSEtool_combine(SEXP listSEXP) {
@@ -225,7 +207,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_MSEtool_bhnoneq_LL", (DL_FUNC) &_MSEtool_bhnoneq_LL, 8},
     {"_MSEtool_combine", (DL_FUNC) &_MSEtool_combine, 1},
     {"_MSEtool_get_freq", (DL_FUNC) &_MSEtool_get_freq, 4},
     {"_MSEtool_get_freq2", (DL_FUNC) &_MSEtool_get_freq2, 3},
