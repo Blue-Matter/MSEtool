cc851ee791d09c0f9e0019270e833211f7dcaa38
Adrian <ar.hordyk@gmail.com>
2021-06-04 10:08:30 -0700

    Update SampleOM.R

--- R/SampleOM.R
+++ R/SampleOM.R
@@ -686,11 +686,11 @@ SampleStockPars <- function(Stock, nsim=48, nyears=80, proyears=50, cpars=NULL, 
   if (all(M_ageArray[1,,1] == mean(M_ageArray[1,,1])) & all(M !=0)) { # constant M at age
     calcMax <- ceiling(-log(0.01)/(min(M)))        # Age at which 1% of cohort survives
     if (maxage < 0.95*calcMax && msg) {
-      message("Note: Maximum age (",
+      message("Note: Maximum age", paste0("(",
               maxage,
-              ") is lower than assuming 1% of cohort survives to maximum age (",
-              calcMax,
-              ")")
+              ")"), "is lower than assuming 1% of cohort survives to maximum age",
+              paste0("(",
+              calcMax,")"))
     }
   }
 
