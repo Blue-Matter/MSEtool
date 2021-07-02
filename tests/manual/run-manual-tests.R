a2c02a8d3d645ccc18c60649720f05ef3267e5e6
Adrian <ar.hordyk@gmail.com>
2021-07-02 15:27:22 -0700

    Update run-manual-tests.R

--- tests/manual/run-manual-tests.R
+++ tests/manual/run-manual-tests.R
@@ -22,11 +22,11 @@ testthat::test_file("tests/manual/test-code/test-checkPopDynamics.R") # ok
 
 testthat::test_file("tests/manual/test-code/test-Data2csv.R") # ok
 
-testthat::test_file("tests/manual/test-code/test-Data_Functions.R") # ok
+testthat::test_file("tests/manual/test-code/test-Data_Functions.R") #  ok 
 
 testthat::test_file("tests/manual/test-code/test-Data_Plotting.R") # ok
 
-testthat::test_file("tests/manual/test-code/test-Fease_Functions.R") # ok
+testthat::test_file("tests/manual/test-code/test-Fease_Functions.R") # ok 
 
 testthat::test_file("tests/manual/test-code/test-Import_Data.R") # ok
 
@@ -38,15 +38,15 @@ testthat::test_file("tests/manual/test-code/test-MSE_Plotting.R") # cosewic and 
 
 testthat::test_file("tests/manual/test-code/test-OM_functions.R") # ok
 
-testthat::test_file("tests/manual/test-code/test-OM_init_doc.R") #
+testthat::test_file("tests/manual/test-code/test-OM_init_doc.R") # ok 
 
-testthat::test_file("tests/manual/test-code/test-OM_Plotting.R") # ok
+testthat::test_file("tests/manual/test-code/test-OM_Plotting.R") # ok (ish)
 
 testthat::test_file("tests/manual/test-code/test-PMobjects.R") # ok
 
 testthat::test_file("tests/manual/test-code/test-RealIndices.R") # ok
 
-testthat::test_file("tests/manual/test-code/test-runMSE.R") # ok
+testthat::test_file("tests/manual/test-code/test-runMSE.R") # 
 
-testthat::test_file("tests/manual/test-code/test-slotDescription.R") # ok
+testthat::test_file("tests/manual/test-code/test-slotDescription.R") # 
 
