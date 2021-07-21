856e7b3039b9b4cdc6ae5c3868bb72952c95eae1
Adrian <ar.hordyk@gmail.com>
2021-07-21 08:37:44 -0700

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,11 +6,6 @@
 
 using namespace Rcpp;
 
-#ifdef RCPP_USE_GLOBAL_ROSTREAM
-Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
-Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
-#endif
-
 // vecminInd
 int vecminInd(NumericVector x);
 RcppExport SEXP _MSEtool_vecminInd(SEXP xSEXP) {
