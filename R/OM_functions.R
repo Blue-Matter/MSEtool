b4c647436e0ed76001d3bef8b57cb8f99c3fe06b
Adrian Hordyk <ar.hordyk@gmail.com>
2022-12-23 11:08:25 -0800

    Update OM_functions.R

--- R/OM_functions.R
+++ R/OM_functions.R
@@ -65,9 +65,9 @@ SubOM <- function(OM, Sub=c("Stock", "Fleet", "Obs", "Imp")) {
 #' OM2 <- Replace(OM, Generic_DecE)
 #'
 #' # Replace Fleet from another OM
-#' OM1 <- new("OM", Albacore, Generic_DecE, Perfect_Info, Overages)
-#' OM2 <- new("OM", Blue_shark, Generic_IncE, Generic_Obs, Perfect_Imp)
-#' OM1a <- Replace(OM1, OM2, "Fleet")
+#' # OM1 <- new("OM", Albacore, Generic_DecE, Perfect_Info, Overages)
+#' # OM2 <- new("OM", Blue_shark, Generic_IncE, Generic_Obs, Perfect_Imp)
+#' # OM1a <- Replace(OM1, OM2, "Fleet")
 #'
 #' @export
 Replace <- function(OM, from,Sub=c("Stock", "Fleet", "Obs", "Imp"),  Name=NULL, silent=FALSE) {
