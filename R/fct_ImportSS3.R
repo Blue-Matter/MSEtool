ec4655953a841ee58827aa644ba5d74c1769cf08
Adrian Hordyk <ar.hordyk@gmail.com>
2025-12-17 09:51:19 -0800

    Update fct_ImportSS3.R

--- R/fct_ImportSS3.R
+++ R/fct_ImportSS3.R
@@ -720,9 +720,13 @@ GetSS_R0 <- function(st, replist, YearsList) {
   
   birthseas <- GetSSBirthSeas(replist)
                       
-  R0 <- dplyr::filter(replist$natage, Sex == st, `Beg/Mid` == "B", Era == "VIRG",
+  R0 <- dplyr::filter(replist$natage, Sex == st, 
+                      `Beg/Mid` == "B", 
+                      Era == "VIRG",
                       Seas==birthseas) |>
-    tidyr::pivot_longer(as.character(AgeClasses), names_to = 'Age', values_to = 'Number') |>
+    tidyr::pivot_longer(as.character(AgeClasses), 
+                        names_to = 'Age', 
+                        values_to = 'Number') |>
     dplyr::mutate(Age=as.numeric(Age)) |>
     dplyr::filter(Age==min(Age)) |>
     dplyr::pull(Number)
