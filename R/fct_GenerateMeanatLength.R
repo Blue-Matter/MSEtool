ddb1f5983f6ffb386a3c8f59b54488963187c631
AdrianHordyk <ar.hordyk@gmail.com>
2026-01-05 09:46:21 -0800

    Update fct_GenerateMeanatLength.R

--- R/fct_GenerateMeanatLength.R
+++ R/fct_GenerateMeanatLength.R
@@ -1,39 +1,29 @@
-#' Generate `MeanAtLength` Values
-#'
-#' @param Model Either the name of a built-in model (character) or a valid R function
-#' @param Pars A `list` of named parameters for `Model`
-#' @param Length A numeric vector of length classes
-#'
-#' @export
-GenerateMeanatLength <- function(Model, Pars, Length) {
-  if (inherits(Model, "function")) {
-    return(ApplyCustomAtLengthModel(Model, Pars, Length))
-  }
-
+GenerateMeanGeneric <- function(Model, Pars, ...) {
+  
   fun_args <- names(formals(Model))
   fun <- get(Model)
   arg_ind <- match(names(Pars), fun_args)
   val_ind <- 1:max(min(arg_ind - 1), 1)
-
+  
+  DotsList <- list(...)
+  
   ParsList <- list()
   for (i in seq_along(val_ind)) {
-    ParsList[[fun_args[[val_ind[i]]]]] <- get(fun_args[[i]])
+    ParsList[[fun_args[[val_ind[i]]]]] <- DotsList[[fun_args[[i]]]]
   }
-
+  
   AreaDimension <- purrr::map(Pars, \(Par) {
     dimnames(Par)[["Area"]]
   }) |> unlist()
-
+  
   if (is.null(AreaDimension)) {
     # no area dimension
-
     for (i in seq_along(arg_ind)) {
       ParsList[[fun_args[[arg_ind[i]]]]] <- Pars[[i]]
     }
-
     return(do.call(fun, ParsList))
   }
-
+  
   nArea <- AreaDimension |>
     as.numeric() |>
     max()
@@ -45,6 +35,23 @@ GenerateMeanatLength <- function(Model, Pars, Length) {
     AreaValues[[area]] <- do.call(fun, ParsList)
   }
   abind::abind(AreaValues, along=4)
+  
+}
+
+
+
+#' Generate `MeanAtLength` Values
+#'
+#' @param Model Either the name of a built-in model (character) or a valid R function
+#' @param Pars A `list` of named parameters for `Model`
+#' @param Length A numeric vector of length classes
+#'
+#' @export
+GenerateMeanatLength <- function(Model, Pars, Length) {
+  if (inherits(Model, "function")) {
+    return(ApplyCustomAtLengthModel(Model, Pars, Length))
+  }
+  GenerateMeanGeneric(Model, Pars, Length=Length)
 }
 
 
@@ -57,10 +64,9 @@ GenerateMeanatWeight <- function(Model, Pars, Weight) {
   
   if (inherits(Model, 'function')) {
     stop("R functions not currently supported for MeanAtWeight")
-    # return(ApplyCustomAtWeightModel(Model, Pars, Length))
+    # return(ApplyCustomAtWeightModel(Model, Pars, Weight))
   }
-  
-  GenerateMeanatLength(Model, Pars, Weight) 
+  GenerateMeanGeneric(Model, Pars, Weight=Weight)
 }
 
 #' @rdname GenerateMeanatLength
@@ -74,5 +80,5 @@ GenerateMeanAtAge <- function(Model, Pars, Ages) {
     return(ApplyCustomAtAgeModel(Model, Pars, Ages))
   }
   
-  GenerateMeanatLength(Model, Pars, Ages) 
+  GenerateMeanGeneric(Model, Pars, Ages=Ages) 
 }
