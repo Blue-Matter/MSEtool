eae52d90d78919a681d44f0d05c3ac9a0d064336
AdrianHordyk <ar.hordyk@gmail.com>
2026-01-05 15:52:32 -0800

    Update 00_Class_ages.R

--- R/00_Class_ages.R
+++ R/00_Class_ages.R
@@ -1,9 +1,6 @@
-#' S4 Object class `ages`
+#' `Ages` Object and Functions
 #' 
-#' `r S4Description('Ages')`
-#' 
-#' ## About the Class
-#' The S4 object class `ages` contain information related to the age classes of a [Stock()]
+#' An `Ages` object contains information related to the age classes of a [Stock()] object.
 #'
 #' ## Creating New Objects
 #' `r Creating_New_Objects('ages')`
