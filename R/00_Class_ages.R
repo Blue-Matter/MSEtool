623036b12a1dadc94e87dd5bb7e74c90ea59d308
Adrian <ar.hordyk@gmail.com>
2026-01-08 23:54:28 -0800

    Update 00_Class_ages.R

--- R/00_Class_ages.R
+++ R/00_Class_ages.R
@@ -1,6 +1,11 @@
 #' `Ages` Object and Functions
 #' 
-#' An `Ages` object contains information related to the age classes of a [Stock()] object.
+#' An `Ages` object defines the age structure associated with a [Stock()]
+#' object. It specifies the minimum and maximum ages, whether a plus group
+#' is used, and the resulting vector of age classes.
+#' 
+#' Age classes are internally represented as numeric values **in units of
+#' years**, and may be fractional (e.g. seasonal or sub-annual ages; rounded to 3 digits).
 #'
 #' ## Creating New Objects
 #' `r Creating_New_Objects('ages')`
@@ -8,7 +13,7 @@
 #' ## Accessing and Assigning Slots
 #' `r Accessing_Assigning_Slots('ages')`
 #'
-#' @seealso [MaxAge()], [MinAge()], []
+#' @seealso [MaxAge()], [MinAge()]
 #' 
 #' @name Ages
 #' 
@@ -16,11 +21,15 @@ NULL
 
 #' @include 00_Class_unions.R
 #' @include 00_Class_child.R
-#' @slot MaxAge Numeric value specifying the maximum age
-#' @slot MinAge Numeric value specifying the minimum age 
-#' @slot Units Character string describing the units of `MaxAge` and `MinAge`
-#' @slot PlusGroup Logical. Use a plusgroup?
-#' @slot Classes Numeric vector of the age classes. **Note:** Age classes are *always* in units of a year
+#' @slot MaxAge Numeric value specifying the maximum age.
+#'   If `PlusGroup = TRUE`, this represents the plus group age.
+#' @slot MinAge Numeric value specifying the minimum age.
+#' @slot Units Character string describing the time units used to define
+#'   age classes (e.g. `"year"`).
+#' @slot PlusGroup Logical; indicates whether the maximum age is treated
+#'   as a plus group.
+#' @slot Classes Numeric vector of age classes expressed in **years**.
+#'   May include fractional values (e.g. `0.25`, `1.5`).
 #' @rdname Ages
 setClass('ages',
          slots=c(MaxAge='numeric',
@@ -30,17 +39,22 @@ setClass('ages',
          contains = c('ClassesClass')
 )
 
-#' @describeIn Ages Create a new `ages` class object
-#' @param MaxAge Either an integer specifying the maximum age in units corresponding to `Units` of a [Stock()], or a [Stock()] class object. If `PlusGroup==TRUE`, `MaxAge` will be a plusgroup. **Required**
-#' @param MinAge An integer specifying the minimum age in units corresponding to `Units` for a [Stock()]. Default is 0. 
-#' @param Units A character string specifying the units of `MaxAge` and `MinAge`. Must be one of `ValidUnits()`
-#' @param PlusGroup Logical. Use a plus group?
+#' @describeIn Ages Create a new `ages` object
+#'
+#' @param MaxAge Either a numeric value specifying the maximum age (in units
+#'   corresponding to `Units`), or a [Stock()] object, in which case the
+#'   existing `Ages` slot is returned.
+#' @param MinAge Numeric value specifying the minimum age. Default is `0`.
+#' @param Units Character string specifying the units used to define age
+#'   classes. Must be one of `ValidUnits()`.
+#' @param PlusGroup Logical; should the maximum age be treated as a plus group?
+#'
 #' @export
 Ages <- function(MaxAge,
                  MinAge=0,
                  Units='year',
                  PlusGroup=TRUE) {
-  if (methods::is(MaxAge, 'stock'))
+  if (inherits(MaxAge, 'stock'))
     return(MaxAge@Ages)
   
   .Object <- methods::new('ages',
@@ -53,9 +67,10 @@ Ages <- function(MaxAge,
   .Object
 }
 
-#' @describeIn Ages Assign an `ages` class object to a [Stock()] object
-#' @param x A [Stock()] class object
-#' @param value An `ages` class object to assign to `x`
+#' @describeIn Ages Assign an [Ages()] object to a [Stock()] object
+#' @param x A [Stock()] object
+#' @param value An [Ages()] object to assign
+#' 
 #' @export
 `Ages<-` <- function(x, value) {
   assignSlot(x, value, 'Ages')
@@ -65,17 +80,17 @@ Ages <- function(MaxAge,
 setValidity('ages', isValidObject)
 
 setMethod("initialize", "ages", function(.Object,
-                                         MaxAge=NA,
+                                         MaxAge=NA_real_,
                                          MinAge=0,
                                          Units='year',
                                          PlusGroup=TRUE) {
   .Object@MinAge <- MinAge
   .Object@Units <- Units
+  .Object@PlusGroup <- PlusGroup
   if (!is.na(MaxAge)) {
     .Object@MaxAge <- MaxAge
     .Object@Classes <- CalcAgeClasses(.Object)
   }
-  .Object@PlusGroup <- PlusGroup
   .Object
 })
 
