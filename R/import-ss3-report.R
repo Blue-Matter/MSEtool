2bf89cf0fd01556f08bad007eb0b13ad8163370d
Adrian <ar.hordyk@gmail.com>
2026-02-24 15:01:50 -0800

    Update import-ss3-report.R

--- R/import-ss3-report.R
+++ R/import-ss3-report.R
@@ -73,26 +73,8 @@ ImportSSReport <- function(SSDir, parallel=FALSE, silent=FALSE, ...) {
   
   parallel <- CheckParallel(parallel)
 
-  CheckParallel <- function(parallel) {
-    if (!parallel)
-      return(FALSE)
-    current_plan <- future::plan()
-    
-    if (inherits(current_plan, "sequential")) {
-      cli::cli_alert_warning(
-        "{.val parallel = TRUE} requested, but no parallel future plan detected."
-      )
-      cli::cli_text("Initialize a parallel plan first using:")
-      cli::cli_ul()
-      cli::cli_li("e.g: `SetupParallel(workers = 4)`")
-      cli::cli_text("Running sequentially instead (`parallel = FALSE`).")
-      return(FALSE)
-    }
-    parallel
-  }
-  if (parallel) {
-  
-  }
+
+ 
   
   if (!parallel) {
     RepList <- purrr::map(
