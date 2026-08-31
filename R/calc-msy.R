d95137d2d3b866681ba0d9a06c03ca6b6097220f
AdrianHordyk <ar.hordyk@gmail.com>
2026-08-31 07:35:20 -0700

    Update calc-msy.R

--- R/calc-msy.R
+++ R/calc-msy.R
@@ -283,7 +283,9 @@ CalcMSY <- function(Hist,
     nSeason                   = inputs$nSeason,
     SeasonalWeightsList       = inputs$SeasonalWeightsList,
     CalendarYears             = inputs$CalendarYears,
-    RefSeason                 = inputs$RefSeason
+    RefSeason                 = inputs$RefSeason,
+    NPR0List                  = inputs$NPR0List,
+    NPR0_SPList               = inputs$NPR0_SPList
   )
 
 
