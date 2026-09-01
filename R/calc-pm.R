f95164e9086029ca180a6311f14655c7875ffb91
AdrianHordyk <ar.hordyk@gmail.com>
2026-09-01 09:59:52 -0700

    Update calc-pm.R

--- R/calc-pm.R
+++ R/calc-pm.R
@@ -711,7 +711,7 @@ PM_Stability <- function(object, Threshold, Type = c('TAC', 'Removals', 'Landing
   if (!is.null(Years))
     ydf <- ydf[ydf$Year %in% Years, ]
   aav <- .AAV(ydf)
-  .BuildPM(aav, Ref = Threshold, Years = NULL, op = `<`,
+  .BuildPM(aav, Ref = Threshold, Years = NULL, op = `<=`,
            Name = 'Stability',
            Caption = paste0('P(interval-to-interval ', .StabilityLabel[[Type]],
                              ' change < ', Threshold, ')'))
